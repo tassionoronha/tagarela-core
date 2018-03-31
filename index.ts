8fe9598c362b8559d20b7a2b50d723c32da75512
tassio <tassionoronha@gmail.com>
2018-03-31 19:08:00 +0200

    add another default example

--- index.ts
+++ index.ts
@@ -5,3 +5,6 @@ console.log(text.getQuery());
 
 var text2 = new Tagarela('exiba produtos com o nome detergente');
 console.log(text2.getQuery());
+
+var text3 = new Tagarela('exiba meus usuarios');
+console.log(text3.getQuery());
