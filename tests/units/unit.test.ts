e0a8ffbd9781d8336990bf46b7d1f6ed2fe2b9c0
tassio <tassionoronha@gmail.com>
2018-03-31 19:34:57 +0200

    add gettersTests

--- tests/units/unit.test.ts
+++ tests/units/unit.test.ts
@@ -4,11 +4,50 @@ import {Tagarela} from '../../classes/Tagarela';
 var text = new Tagarela('qual o nome e telefone e email dos usuarios que tem cidade Salvador');
 
 describe('Testes unitários da Classe Tagarela', () => {
-
-  describe('Método load model', () => {
-    it('Deve carregar um novo modelo', () => {
-      let tg = text.addModel(User);
-      expect(tg).to.be.an('array');
+    it('Valida se o texto foi carregado', () => {
+      expect(text.getText()).to.be.an('string');
+      expect(text.getText()).to.be.eql('qual o nome e telefone e email dos usuarios que tem cidade Salvador');
     });
-  });
+
+    it('Valida se os modelos foram carregados', () => {
+      expect(text.getModels()).to.be.an('array');
+      expect(text.getModels()).to.not.be.empty;
+    });
+
+    it('Valida se ao menos uma tabela foi carregada', () => {
+      expect(text.getTables()).to.not.be.empty;
+    });
+
+    it('Valida se a palavras foram tokenizadas', () => {
+      expect(text.getWords()).to.not.be.empty;
+      expect(text.getWords()).to.contain('usuarios');
+      expect(text.getWords()).to.contain('cidade');
+    });
+
+    it('Valida se as excessões foram carregadas', () => {
+      expect(text.getExcess()).to.not.be.empty;
+      expect(text.getExcess()).to.contain('a');
+      expect(text.getExcess()).to.contain('o');
+    });
+
+    it('Valida se foram encontradas as devidas projeções', () => {
+      expect(text.getProjections()).to.not.be.empty;
+      expect(text.getProjections()).to.contain('name');
+      expect(text.getProjections()).to.contain('phone');
+      expect(text.getProjections()).to.contain('email');
+    });
+
+    it('Valida se foram carregadas as devidas clausulas', () => {
+      let clausules = text.getClausules();
+      expect(clausules).to.not.be.empty;
+      expect(clausules[0]).to.have.keys(['field', 'comparator', 'searched']);
+    });
+
+    it('Valida as palavras de inferencias foram carregadas', () => {
+      expect(text.getInferences()).to.not.be.empty;
+      expect(text.getInferences()).to.contain('sao');
+      expect(text.getInferences()).to.contain('contem');
+      expect(text.getInferences()).to.contain('é');
+      expect(text.getInferences()).to.contain('tem');
+    });
 });
